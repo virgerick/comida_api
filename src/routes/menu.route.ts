6b91673daf9456b99c799995c731bfd097a9fc73
virgerick <elfeo.0103@gmail.com>
2021-08-05 00:13:29 -0400

    Manejando exceptiones

--- src/routes/menu.route.ts
+++ src/routes/menu.route.ts
@@ -5,25 +5,50 @@ const router = Router();
 const Repostory: MenuRepository = new MenuRepository();
 
 router.get("/menu", async (req, res) => {
- 
-  const result = await Repostory.get(req.query);
-  res.json(result);
+ try {
+     const result = await Repostory.get(req.query);
+     res.status(200).json(result);
+ } catch (error) {
+     res.json(error)
+ }
+  
+  
 });
 router.get("/menu/:id", async (req, res) => {
-  const result = await Repostory.find(req.params.id);
+    try {
+         const result = await Repostory.find(req.params.id);
   res.json(result);
+    } catch (error) {
+        res.json(error)
+    }
+ 
 });
 router.post("/menu/", async (req, res) => {
-  const result = await Repostory.add(req.body);
+    try {
+         const result = await Repostory.add(req.body);
   res.json(result);
+    } catch (error) {
+        res.json(error)
+    }
+ 
 });
 router.put("/menu/:id", async (req, res) => {
-  const result = await Repostory.update(req.params.id,req.body);
-  res.json(result);
+    try {
+        const result = await Repostory.update(req.params.id,req.body);
+        res.json(result);
+    } catch (error) {
+        
+    }
+  
 });
 router.delete("/menu/:id", async (req, res) => {
-  const result = await Repostory.delete(req.params.id);
+    try {
+        const result = await Repostory.delete(req.params.id);
   res.json(result);
+    } catch (error) {
+        res.json(error)
+    }
+  
 });
 
 export default router;
