09111b64849cfc050775f0cc200ef190077a1fc2
virgerick <elfeo.0103@gmail.com>
2021-08-09 21:37:58 -0400

    adding description to menu

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, model } from "mongoose";
+
+export interface Menu extends mongoose.Document {
+    name: string;
+    price: Number;
+    rate: Number;
+    imagen: string;
+};
+const MenuSchema = new Schema({
+    
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value: number) {
+            if (value < 0) throw new Error("Negative price aren't real.");
+        }
+    },
+    rate: {
+        type: Number,
+        required: false,
+        default: 0,
+
+    },
+    imagen: {
+        type: String,
+        required: false,
+        trim: true
+    },
+});
+export default model<Menu>('Menu',MenuSchema)
